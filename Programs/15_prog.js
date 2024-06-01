44638592bca9e28939c7dfbef2527cd5a2044800
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-06-01 18:49:56 +0530

    init

---
+++ Programs/15_prog.js
@@ -0,0 +1,16 @@
+// let name = {
+//     firstName: 'Sandeep',
+//     lastName: 'Singh'
+// }
+
+// console.table(name)
+// console.error(name);
+// console.log(name);
+// console.log(name.firstName);
+// console.log(name.lastName);
+
+
+const Person = {
+    name: 'Sandeep Singh',
+    age: '25',
+}
