bfa3c60800ebde8a8916867c656f1cdfa6a4fe7e
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-08-05 10:27:56 +0530

    Search Insertion Position

---
+++ Programs/27_prog.js
@@ -0,0 +1,25 @@
+// Search Insertion Position
+
+var searchInsert = function (nums, target) {
+    // let left = 0;
+    // let right = nums.length - 1;
+    // while (left <= right) {
+    //     let mid = Math.floor((left + right) / 2);
+    //     if (nums[mid] === target) {
+    //         return mid;
+    //     } else if (nums[mid] < target) {
+    //         left = mid + 1;
+    //     } else {
+    //         right = mid - 1;
+    //     }
+    // }
+    // return left;
+
+    for(let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            return i;
+        }
+    }
+};
+
+console.log(searchInsert([1, 3, 5, 6], 5));
