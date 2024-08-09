2341de9059a0e1c404c8da4093e059fab7c7e638
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-08-09 19:38:37 +0530

    reverse string

---
+++ Programs/32_reverse_string.js
@@ -0,0 +1,10 @@
+// How to reverse string
+
+function Reverse(str) {
+    for (let i = str.length - 1; i >= 0; i--) {
+        console.log(str[i]);
+        
+    }
+}
+
+Reverse("My name is sandeep singh")
