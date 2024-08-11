b10203e30f586e7917a483b3740d03bcffea921e
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-08-11 12:31:40 +0530

    odd numbers

---
+++ Programs/39_Odd_no.js
@@ -0,0 +1,11 @@
+// Display Odd numbers from 1 to 100
+
+function OddNumbers(n) {
+    if (n % 2 != 0)
+        console.log("Odd Number: " + n);
+    else 
+        console.log("Even Number: " + n);
+        
+}
+
+OddNumbers(101)
