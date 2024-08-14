94800f89f084e296a922898af5bd064bd847121c
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-08-14 19:45:31 +0530

    negative and positive no

---
+++ Programs/41.Negative_positive_no.js
@@ -0,0 +1,12 @@
+// Program Negative Positive Number
+
+function NegativePositiveNumber(n) {
+    if (n > 0) {
+        console.log("Positive Numver: ", n);
+    } else if (n < 0) {
+        console.log("Negative Number: ", n);
+        
+    }
+}
+
+NegativePositiveNumber(-10)
