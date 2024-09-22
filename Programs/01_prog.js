7ace066b75624bc8abf3cde3df38060c1c5ea722
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-09-22 11:27:52 +0530

    "Added console logs for basic math operations and assigned values to variables a and b."

--- Programs/01_prog.js
+++ Programs/01_prog.js
@@ -3,3 +3,11 @@
 console.log("Hello World");
 console.log('Sandeep Singh');
 console.log('I am 24 years old');
+
+
+// add two no
+console.log(10 + 20);
+
+const a = 20
+const b = 30
+console.log(a + b);
