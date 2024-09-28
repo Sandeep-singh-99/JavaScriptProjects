5a1e6f3d7b5d75324acf7bcd824becdeb23e4c26
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-09-28 20:58:52 +0530

    javascript time

---
+++ BASIC _or_Advanced_Javascript/20_time.js
@@ -0,0 +1,21 @@
+// function mytime() {
+//     setInterval(() => {
+//         console.log("Sandeep Singh");
+//         console.log(new Date().toLocaleTimeString());
+//     },3000)
+// }
+
+// mytime()
+
+
+
+
+function mytime1() {
+    setTimeout(() => {
+        console.log("Akash Ydav");
+        console.log(new Date().toLocaleTimeString());
+    }, 3000)
+}
+
+mytime1()
+
