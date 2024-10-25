f4c55a4761493faa328bfde0f3e458809a06f47d
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-10-25 14:26:06 +0530

    promises

--- JS/advanced/promises1.js
+++ JS/advanced/promises1.js
@@ -1,6 +1,22 @@
-const a = true
+// const a = true
 
-const myName = new Promise((reslove, reject) => {
+// const myName = new Promise((reslove, reject) => {
+//     if (a) {
+//         reslove('sandeep')
+//     }else {
+//         reject('error')
+//     }
+// })
+
+
+const a = false
+
+// myName
+//     .then((data) => console.log(data))
+//     .catch((error) => console.log(error))
+
+
+const myName1 = new Promise((reslove, reject) => {
     if (a) {
         reslove('sandeep')
     }else {
@@ -8,6 +24,6 @@ const myName = new Promise((reslove, reject) => {
     }
 })
 
-myName
+myName1
     .then((data) => console.log(data))
     .catch((error) => console.log(error))
