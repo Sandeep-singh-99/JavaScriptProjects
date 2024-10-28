6cb86eca364b82c16a9cf99a3ef50de3c0918469
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-10-28 14:13:20 +0530

    update

--- LeetCode Solution/Missing_Number.js
+++ LeetCode Solution/Missing_Number.js
@@ -1,16 +1,16 @@
-var missingNumber = function(nums) {
-    let n = nums.length;
-    let sum = 0;
-    for (let i = 0; i <= n-1; i++) {
-        sum = sum + nums[i];
-    }
+var missingNumber = function (nums) {
+  let n = nums.length;
+  let sum = 0;
+  for (let i = 0; i <= n - 1; i++) {
+    sum = sum + nums[i];
+  }
 
-    let ActualSum = (n * (n+1))/2;
+  let ActualSum = (n * (n + 1)) / 2;
 
-    let missingnum = ActualSum - sum;
-    return missingnum
-}
+  let missingnum = ActualSum - sum;
+  return missingnum;
+};
 
-nums = [9,6,4,2,3,5,7,0,1]
+nums = [9, 6, 4, 2, 3, 5, 7, 0, 1];
 
-console.log(missingNumber(nums)) // 2
+console.log(missingNumber(nums)); // 2
