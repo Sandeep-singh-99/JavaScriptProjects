5a0752cb9b3c232aa6684b8ff6eeabd0573fdc11
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-11-27 19:52:30 +0530

    add initial array logging implementations in 01_prog.js and 02_prog.js

---
+++ DSA/Array/01_prog.js
@@ -0,0 +1,5 @@
+const arr = [90, 55, 5, 35, 2, 89, 1]
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i] + " ");
+}
---
+++ DSA/Array/02_prog.js
@@ -0,0 +1,7 @@
+const arr = [90, 55, 5, 35, 2, 89, 1]
+
+arr.forEach((element) => {
+    console.log(element);
+});
+
+console.log("Index: ",arr[2]);
