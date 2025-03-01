0c5ee58e5e1745cc40f90750a56365715d5e34b8
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-03-01 12:02:31 +0530

    add initial HTML, CSS, and JavaScript files with basic structure and odd/even number logging

---
+++ pratice/02/script.js
@@ -0,0 +1,28 @@
+// console.log(40*2-5);
+
+
+const currentDate = new Date()
+
+console.log("Day:",currentDate.getDay());
+
+console.log("Month:", currentDate.getMonth())
+
+console.log("Year:", currentDate.getFullYear())
+
+console.log("Hours:", currentDate.getHours())
+
+console.log("Minutes:", currentDate.getMinutes())
+
+console.log("Seconds:", currentDate.getSeconds())
+
+console.log("Milliseconds:", currentDate.getMilliseconds())
+
+console.log("Time:", currentDate.getTime())
+
+console.log("Date:", currentDate.getDate())
+
+console.log("TimezoneOffset:", currentDate.getTimezoneOffset())
+
+console.log("UTCDate:", currentDate.getUTCDate())
+
+
