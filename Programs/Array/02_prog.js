77f0ff9d81fcc18db3307b3226056985894f9acd
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-03-17 00:34:56 +0530

    add program to print the elements of an array

---
+++ Programs/Array/02_prog.js
@@ -0,0 +1,9 @@
+// Program to print the elements of an array
+
+function printArray(arr) {
+    console.log("Array is:", arr);
+}
+
+arr = [3, 8, 7, 6, 5, -4, 3, 2, 1];
+
+printArray(arr); // [3, 8, 7, 6, 5, -4, 3, 2, 1]
