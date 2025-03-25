5faafa40d0d7229596411a845af250ef44fc69ba
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-03-25 23:02:34 +0530

    add programs to demonstrate asynchronous logging and object manipulation

---
+++ Programs/75_prog.js
@@ -0,0 +1,14 @@
+let obj = {
+    id: "1",
+    name: "Sandeep",
+    age: "25",
+    work: "programmer"
+}
+
+console.log(Object.keys(obj));
+
+console.log(Object.values(obj));
+
+console.log(Object.entries(obj));
+
+
