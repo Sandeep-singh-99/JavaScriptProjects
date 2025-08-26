1345e0beacd971452241371322ea9bd07f920e7d
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-08-26 11:29:40 +0530

    add: implement LinkedListPrint class with methods for adding nodes and printing the list

---
+++ DSA/LinkedList/06.js
@@ -0,0 +1,41 @@
+// Singly linked list Examples
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedListPrint {
+    constructor() {
+        this.head = null;
+    }
+
+    add(data) {
+        const newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    printList() {
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+const linkedList = new LinkedListPrint();
+linkedList.add(1);
+linkedList.add(2);
+linkedList.add(30);
+linkedList.printList();
