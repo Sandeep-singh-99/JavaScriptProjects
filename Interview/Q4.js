9171fd6eba3b756f46ef7f65aa787c8f592d3ed9
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-10-22 22:17:00 +0530

    add: implement setTimeout and setInterval functions for delayed greetings

---
+++ Interview/Q4.js
@@ -0,0 +1,16 @@
+// setTimeOut
+
+
+function fun() {
+    console.log("Hello, World!");
+}
+
+setTimeout(fun, 1000);
+
+// SetInterval
+
+function greet() {
+    console.log("Hi there!");
+}
+
+setInterval(greet, 2000);
