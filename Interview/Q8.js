1cb656efee66c7a38b86dfb984c9f0f6f53e9598
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-11-09 11:25:06 +0530

    add: create Q8.js with an object and console logging examples

---
+++ Interview/Q8.js
@@ -0,0 +1,17 @@
+let obj = {
+    a: "Hello, world!",
+    b: 42,
+    c: false
+}
+
+console.log(obj.a);
+
+console.log(obj.b);
+
+console.log(obj.c);
+
+console.log(obj["a"]);
+
+console.log(obj["b"]);
+
+console.log(obj["c"]);
