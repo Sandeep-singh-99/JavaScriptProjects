8204b5fadb7c00b63b75c5154e91805daed58064
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-11-09 11:18:24 +0530

    add: implement type checking for various data types in Q6.js

---
+++ Interview/Q5.js
@@ -0,0 +1,7 @@
+let a = 42
+let b = '42'
+
+console.log(a == b);
+
+console.log(a === b);
+
---
+++ Interview/Q6.js
@@ -0,0 +1,28 @@
+let a;
+
+console.log(typeof a);
+
+let b = "Hello, World!";
+console.log(typeof b);
+
+let c = 100;
+console.log(typeof c);
+
+
+let d = true;
+console.log(typeof d);
+
+let e = null;
+console.log(typeof e);
+
+let f = undefined;
+console.log(typeof f);
+
+let g = { name: "Alice", age: 30 };
+console.log(typeof g);
+
+let h = [1, 2, 3, 4, 5];
+console.log(typeof h);
+
+let i = function() { return "I am a function"; };
+console.log(typeof i);
